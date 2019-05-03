3f8b736f0a78bbde34ab2fb047d02f7e0a0f9632
JonesQuito <jonesdhy@hotmail.com>
2019-05-03 00:25:35 -0300

    database de teste

---
+++ database.js
@@ -0,0 +1,53 @@
+var dados = 
+{
+    pessoa :{
+        cd_pessoa_fisica: 251487,
+        nm_pessoa_fisica: "Saru Tobi",
+        dt_nascimento: "1990-02-12",
+        sexo: 'Maculino',
+        nr_cpf: "236.125.124.20",
+        nr_identidade: "45821596",
+        telefone: "(62) 99585-2654",
+        email: 'sarutobi@naruto.com'
+    },
+    
+    especialidades : [
+        {descricao: "Clinica Geral"},
+        {descricao: "Cardiologia"},
+        {descricao: "Ginecologia"},
+        {descricao: "Urologia"},
+        {descricao: "Pediatria"},
+        {descricao: "Psicologia"},
+        {descricao: "Psiquiatria"},
+        {descricao: "Mastologia"},
+        {descricao: "Cirurgião Geral"},
+        {descricao: "Neorologia"},
+    ],
+    medicos : [
+        {nome: "Jaqueline"},
+        {nome: "Nicole"},
+        {nome: "Felipe"},
+        {nome: "Larissa"},
+        {nome: "Juliana"},
+        {nome: "Ludimila"},
+        {nome: "Julio"},
+        {nome: "Francisco"},
+        {nome: "Roberto"},
+        {nome: "Gabriel"},
+    ],
+    vagas: [
+        {data: '12/05', vaga: 2},
+        {data: '13/05', vaga: 1},
+        {data: '16/05', vaga: 7},
+        {data: '17/05', vaga: 8},
+        {data: '19/05', vaga: 10},
+        {data: '23/05', vaga: 9},
+        {data: '24/05', vaga: 7},
+        {data: '25/05', vaga: 8},
+        {data: '27/05', vaga: 12},
+    ]
+}
+
+
+
+module.exports = dados;
